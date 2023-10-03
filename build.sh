7b49e94e5496b0baaa85e76279b60f7d86974a2a
kasunJKD <kasun.ravihara@gmail.com>
2023-10-03 21:45:30 +0530

    linux build script

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-libs=-luser32
-warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations -Wno-switch"
+timestamp=$(date +%s)
 
-clang++ -g src/main.cpp -o ./build/game.exe $libs $warnings
+warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations -Wno-switch"
+includes="-Ithird_party -Ithird_party/Include"
+
+# Replace Windows-specific libraries with their Linux equivalents
+libs="-lX11 -lGL -lGLU"
+
+# Build the executable for Linux
+g++ $includes -g src/main.cpp -o schnitzel $libs $warnings
+
+# Remove old game_* files
+rm -f game_*
+
+# Build the shared library for Linux
+g++ -shared -fPIC -g src/game.cpp -o game_$timestamp.so $warnings
+mv game_$timestamp.so game.so
